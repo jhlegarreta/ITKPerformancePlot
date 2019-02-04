6521740f951dbe046979a39e8cdb404905efcd30
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-02-03 20:05:06 -0500

    DOC: Conform to Sphinx docstring guidelines.
    Conform to Sphinx docstring guidelines.
    
    Take advantage of the commit to improve the documentation style, and use
    the PEP8 line length limit.

--- visualization/heatmap_utils.py
+++ visualization/heatmap_utils.py
@@ -15,43 +15,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# Based on the matplotlib tutorial
+# https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def heatmap(data, row_labels, col_labels, ax=None,
-            cbar_kw={}, cbarlabel="", **kwargs):
+def heatmap(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="",
+            **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
-    Arguments:
-        data       : A 2D numpy array of shape (N,M)
-        row_labels : A list or array of length N with the labels
-                     for the rows
-        col_labels : A list or array of length M with the labels
-                     for the columns
-    Optional arguments:
-        ax         : A matplotlib.axes.Axes instance to which the heatmap
-                     is plotted. If not provided, use current axes or
-                     create a new one.
-        cbar_kw    : A dictionary with arguments to
-                     :meth:`matplotlib.Figure.colorbar`.
-        cbarlabel  : The label for the colorbar
+    Parameters
+    ----------
+        data : 2D array
+            A 2D numpy array of shape (N,M).
+        row_labels : 1D array
+            A list or array of length N with the labels for the rows.
+        col_labels : 1D array
+            A list or array of length M with the labels for the columns.
+        ax : matplotlib.axes.Axes, optional
+            A matplotlib.axes.Axes instance to which the heatmap is plotted.
+            If None, use current axes or create a new one.
+        cbar_kw : dict, optional
+            A dictionary with arguments to :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel : string, optional
+            The label for the colorbar.
+
     All other arguments are directly passed on to the imshow call.
     """
 
     if not ax:
         ax = plt.gca()
 
-    # Plot the heatmap
+    # Plot the heatmap.
     im = ax.imshow(data, **kwargs)
 
-    # Create colorbar
+    # Create colorbar.
     cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
     cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
-    # We want to show all ticks...
+    # We want to show all ticks.
     ax.set_xticks(np.arange(data.shape[1]))
     ax.set_yticks(np.arange(data.shape[0]))
     # ... and label them with the respective list entries.
@@ -59,8 +66,7 @@ def heatmap(data, row_labels, col_labels, ax=None,
     ax.set_yticklabels(row_labels)
 
     # Let the horizontal axes labeling appear on top.
-    ax.tick_params(top=True, bottom=False,
-                   labeltop=True, labelbottom=False)
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
 
     # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
@@ -79,24 +85,27 @@ def heatmap(data, row_labels, col_labels, ax=None,
 
 
 def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
-                     textcolors=["black", "white"],
-                     threshold=None, **textkw):
+                     textcolors=["black", "white"], threshold=None, **textkw):
     """
     A function to annotate a heatmap.
 
-    Arguments:
-        im         : The AxesImage to be labeled.
-    Optional arguments:
-        data       : Data used to annotate. If None, the image's data is used.
-        valfmt     : The format of the annotations inside the heatmap.
-                     This should either use the string format method, e.g.
-                     "$ {x:.2f}", or be a :class:`matplotlib.ticker.Formatter`.
-        textcolors : A list or array of two color specifications. The first is
-                     used for values below a threshold, the second for those
-                     above.
-        threshold  : Value in data units according to which the colors from
-                     textcolors are applied. If None (the default) uses the
-                     middle of the colormap as separation.
+    Parameters
+    ----------
+        im : AxesImage
+            The AxesImage to be labeled.
+        data : data, optional
+            Data used to annotate. If None, the image's data is used.
+        valfmt : string or matplotlib.ticker.Formatter, optional
+            The format of the annotations inside the heatmap. This should
+            either use the string format method, e.g. "$ {x:.2f}", or be a
+            :class:`matplotlib.ticker.Formatter`.
+        textcolors : 1D array, optional
+            A list or array of two color specifications. The first is used for
+            values below a threshold, the second for those above.
+        threshold  : real, optional
+            Value in data units according to which the colors from textcolors
+            are applied. If None (the default) uses the middle of the colormap
+            as separation.
 
     Further arguments are passed on to the created text labels.
     """
@@ -110,13 +119,12 @@ def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
     else:
         threshold = im.norm(data.max())/2.
 
-    # Set default alignment to center, but allow it to be
-    # overwritten by textkw.
-    kw = dict(horizontalalignment="center",
-              verticalalignment="center")
+    # Set default alignment to center, but allow it to be overwritten by
+    # textkw.
+    kw = dict(horizontalalignment="center", verticalalignment="center")
     kw.update(textkw)
 
-    # Get the formatter in case a string is supplied
+    # Get the formatter in case a string is supplied.
     if isinstance(valfmt, str):
         valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
 
